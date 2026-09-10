9bc27fa7e12a08eef21f2fcb7c3139cebf2e6fa8
jhiconx <jhudson@iconx.io>
2026-09-10 15:38:12 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE='gft-offer-wall-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/offerwall-logo-lockup.jpg','./assets/offerwall-icon-192.png','./assets/offerwall-icon-512.png','./assets/chili-rewards-mark.png'];
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/offerwall-logo-horizontal.png','./assets/offerwall-icon-192.png','./assets/offerwall-icon-512.png','./assets/chili-rewards-mark.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request))));
